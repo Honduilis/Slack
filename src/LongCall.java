6d990797217ec1d42d91a2929fa37e0001bf2f5d
Honduilis <64107457+Honduilis@users.noreply.github.com>
2020-05-11 17:34:38 -0400

    Update LongCall.java

--- src/LongCall.java
+++ src/LongCall.java
@@ -2,5 +2,6 @@ public class LongCall {
     public static void main(String[] args) {
         System.out.println("Practicing git");
         // writing from git page 
+        // it is been very fun this last 3 months of learning process
     }
 }

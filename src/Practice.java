f1824ceb055eaee71ce0f3daf2377a5f047b0c93
Honduilis <64107457+Honduilis@users.noreply.github.com>
2020-05-11 17:39:21 -0400

    Update Practice.java

--- src/Practice.java
+++ src/Practice.java
@@ -2,6 +2,7 @@ public class Practice {
     public static void main(String[] args) {
         for (int i =0; i <5; i++)
         System.out.println("Ilis Escoto");
+        // this is a regular for loop to print 5 times 
 
     }
 }
